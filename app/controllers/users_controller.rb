ff27a7ed3e9c6a04e1d4238df91d86e27f7d40d8
Justin Berger Howes <howes.j.j@gmail.com>
2021-10-28 23:14:59 +1100

    initial navbar set up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,47 +1,46 @@
 class UsersController < ApplicationController
+  def index
+    @users = User.all
+  end
+
   def new
-    def index
-      @users = User.all
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(users_params)
+
+    if @user.save
+      redirect_to user_path(@user)
+    else
+      render :new
     end
-  
-    def new
-      @user = User.new
+
+  end
+
+  def update
+    @user = User.find(params[:id])
+
+    if @user.update(users_params)
+      redirect_to user_path(@user)
+    else
+      render :edit
     end
-  
-    def create
-      @user = User.new(users_params)
-  
-      if @user.save
-        redirect_to user_path(@user)
-      else
-        render :new
-      end
-  
-    end
-  
-    def update
-      @user = User.find(params[:id])
-  
-      if @user.update(users_params)
-        redirect_to user_path(@user)
-      else
-        render :edit
-      end
-    end
-  
-    def edit
-      @user = User.find(params[:id])
-    end
-  
-    def destroy
-      @user = User.find(params[:id])
-      @user.destroy
-      redirect_to users_path
-    end
-  
-    private
-  
-    def users_params
-      params.require(:user).permit(:name)
-    end
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+    redirect_to users_path
+  end
+
+  private
+
+  def users_params
+    params.require(:user).permit(:name)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,79 +1,11 @@
 Rails.application.routes.draw do
-  get 'users/new'
+  root to: 'cities#index'
 
-  get 'users/create'
-
-  get 'users/edit'
-
-  get 'users/update'
-
-  get 'users/destroy'
-
-  get 'users/users_params'
-
-  get 'reviews/new'
-
-  get 'reviews/create'
-
-  get 'reviews/edit'
-
-  get 'reviews/update'
-
-  get 'reviews/destroy'
-
-  get 'reviews/reviews_params'
-
-  get 'reservations/new'
-
-  get 'reservations/edit'
-
-  get 'reservations/create'
-
-  get 'reservations/update'
-
-  get 'reservations/destroy'
-
-  get 'reservations/reservations_params'
-
-  get 'neighborhoods/new'
-
-  get 'neighborhoods/edit'
-
-  get 'neighborhoods/create'
-
-  get 'neighborhoods/update'
-
-  get 'neighborhoods/destroy'
-
-  get 'listings/new'
-
-  get 'listings/create'
-
-  get 'listings/edit'
-
-  get 'listings/update'
-
-  get 'listings/destroy'
-
-  get 'listing/new'
-
-  get 'listing/create'
-
-  get 'listing/edit'
-
-  get 'listing/update'
-
-  get 'listing/destroy'
-
-  get 'cities/new'
-
-  get 'cities/create'
-
-  get 'cities/update'
-
-  get 'cities/edit'
-
-  get 'cities/destroy'
-
+resources :cities
+resources :listings
+resources :neighborhoods
+resources :reservations
+resources :reviews
+resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
